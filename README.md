db70360b34753418e968ec38e862eebb4b24213d
FlyingTopHat <lucas@flyingtophat.co.uk>
2013-04-30 00:21:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple Tabs (jQuery plugin)
+
+A simple jQuery plugin for creating tabs, which have the added benefit of gracefully degrading if JavaScript isn't available.
+
+## Example usage
+    <div>
+        <ul id="tab-nav">
+            <li><a href="#tabs-1">Example Tab 1</a></li>
+            <li><a href="#tabs-2">Example Tab 2</a></li>
+        </ul>
+   
+        <div id="tab-1">Example text for tab body 1</div>
+        <div id="tab-2">Example text for tab body 2</div>
+    </div>
+    
+    <script type="text/javascript">
+        $("#tab-nav").tabmenu();
+    </script>
---
+++ jquery.tabmenu.js
@@ -0,0 +1,33 @@
+(function ($) {
+    $.fn.tabmenu = function () {
+		var _changeTab = function(parent, selectedTabBodyId){
+		
+			$(parent).find('li a').each(function(index, element){
+				var targetId = $(element).attr('href');
+				
+				if(targetId === selectedTabBodyId) {
+					$(element).parent().addClass('active');
+					$(targetId).show();
+				} else {
+					$(element).parent().removeClass('active');
+					$(targetId).hide();
+				}
+			});
+			
+		}
+
+        this.find('li a').click(function () {
+			var listItemParent = $(this).parent('li').parent(); //Allows plugin to work on ul and ol
+			var selectedTabBodyId = $(this).attr('href');
+			
+			_changeTab(listItemParent, selectedTabBodyId);
+            return false;
+        });
+		
+		// Open first tab
+		_changeTab(
+			this,
+			this.find('li a:first').attr('href')
+		);
+    };
+})(jQuery);
